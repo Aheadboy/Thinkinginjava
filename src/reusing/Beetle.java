5af7166eb44b636be983cfceb242564984bb8422
pure <502964379@qq.com>
2019-03-12 21:11:10 +0800

    1、多态

--- src/reusing/Beetle.java
+++ src/reusing/Beetle.java
@@ -1,6 +1,8 @@
 package reusing;//: reusing/Beetle.java
 // The full process of initialization.
-
+//加载类首先加载其父类，不管父类有没有被实例化。如果父类还有父类，以此类推
+//加载的时候初始化static代码块，按其在源码中的顺序进行。
+//实例化一个类，先实例化其父类，如果父类还有父类，以此类推。
 import static net.mindview.util.Print.*;
 
 class Insect {
