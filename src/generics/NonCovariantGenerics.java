043b507ed07c2eb20aec65fea434b19b8d3e07e9
pure <502964379@qq.com>
2019-03-12 16:10:55 +0800

    1、部分修改，使程序能够通过编译

--- src/generics/NonCovariantGenerics.java
+++ src/generics/NonCovariantGenerics.java
@@ -1,8 +1,9 @@
-//: generics/NonCovariantGenerics.java
-// {CompileTimeError} (Won't compile)
-import java.util.*;
-
-public class NonCovariantGenerics {
-  // Compile Error: incompatible types:
-  List<Fruit> flist = new ArrayList<Apple>();
-} ///:~
+//ljj1这个无法通过编译，是作者的本意
+//package generics;//: generics/NonCovariantGenerics.java
+//// {CompileTimeError} (Won't compile)
+//import java.util.*;
+//
+//public class NonCovariantGenerics {
+//  // Compile Error: incompatible types:
+//  List<Fruit> flist = new ArrayList<Apple>();
+//} ///:~
--- src/generics/UseList.java
+++ src/generics/UseList.java
@@ -1,8 +1,9 @@
-//: generics/UseList.java
-// {CompileTimeError} (Won't compile)
-import java.util.*;
-
-public class UseList<W,T> {
-  void f(List<T> v) {}
-  void f(List<W> v) {}
-} ///:~
+//ljj1 无法通过编译，这是作者的本意
+//package generics;//: generics/UseList.java
+//// {CompileTimeError} (Won't compile)
+//import java.util.*;
+//
+//public class UseList<W,T> {
+//  void f(List<T> v) {}
+//  void f(List<W> v) {}
+//} ///:~
