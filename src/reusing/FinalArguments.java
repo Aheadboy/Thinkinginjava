58c4853550760f761ab0b1f1ed8d01a0cc7a7851
pure <502964379@qq.com>
2019-03-19 11:27:31 +0800

    final 关键字 1、final 函数参数，主要用于匿名内部类，其他情况没什么意义。

--- src/reusing/FinalArguments.java
+++ src/reusing/FinalArguments.java
@@ -1,24 +1,30 @@
-//: reusing/FinalArguments.java
+package reusing;//: reusing/FinalArguments.java
 // Using "final" with method arguments.
-
+//final参数，主要用于匿名内部类。
 class Gizmo {
-  public void spin() {}
+    public void spin() {
+    }
 }
 
 public class FinalArguments {
-  void with(final Gizmo g) {
-    //! g = new Gizmo(); // Illegal -- g is final
-  }
-  void without(Gizmo g) {
-    g = new Gizmo(); // OK -- g not final
-    g.spin();
-  }
-  // void f(final int i) { i++; } // Can't change
-  // You can only read from a final primitive:
-  int g(final int i) { return i + 1; }
-  public static void main(String[] args) {
-    FinalArguments bf = new FinalArguments();
-    bf.without(null);
-    bf.with(null);
-  }
+    public static void main(String[] args) {
+        FinalArguments bf = new FinalArguments();
+        bf.without(null);
+        bf.with(null);
+    }
+
+    void with(final Gizmo g) {
+        //! g = new Gizmo(); // Illegal -- g is final
+    }
+
+    void without(Gizmo g) {
+        g = new Gizmo(); // OK -- g not final
+        g.spin();
+    }
+
+    // void f(final int i) { i++; } // Can't change
+    // You can only read from a final primitive:
+    int g(final int i) {//个人感觉基本类型final参数好像没什么用。
+        return i + 1;
+    }
 } ///:~
