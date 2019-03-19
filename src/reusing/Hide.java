67274ae2bac059dcb31d09e504cbf9aacbc8d79e
pure <502964379@qq.com>
2019-03-19 09:28:09 +0800

    1、pdf做笔记

--- src/reusing/Hide.java
+++ src/reusing/Hide.java
@@ -1,35 +1,47 @@
-//: reusing/Hide.java
+package reusing;//: reusing/Hide.java
 // Overloading a base-class method name in a derived
 // class does not hide the base-class versions.
+
 import static net.mindview.util.Print.*;
 
+//If a Java base class has a method name that’s overloaded several times,
+//        redefining that        method name in the derived class will not hide any of the base-class versions
 class Homer {
-  char doh(char c) {
-    print("doh(char)");
-    return 'd';
-  }
-  float doh(float f) {
-    print("doh(float)");
-    return 1.0f;
-  }
+    char doh(char c) {
+        print("doh(char)");
+        return 'd';
+    }
+
+    float doh(float f) {
+        print("doh(float)");
+        return 1.0f;
+    }
 }
 
-class Milhouse {}
+class Milhouse {
+}
 
 class Bart extends Homer {
-  void doh(Milhouse m) {
-    print("doh(Milhouse)");
-  }
+    void doh(Milhouse m) {
+        print("doh(Milhouse)");
+    }
+
+    @Override
+    char doh(char c) {
+        print("我就呵呵了");
+
+        return 'l';
+    }
 }
 
 public class Hide {
-  public static void main(String[] args) {
-    Bart b = new Bart();
-    b.doh(1);
-    b.doh('x');
-    b.doh(1.0f);
-    b.doh(new Milhouse());
-  }
+    public static void main(String[] args) {
+        Bart b = new Bart();
+        b.doh(1);
+        b.doh('x');
+        b.doh(1.0f);
+        b.doh(new Milhouse());
+    }
 } /* Output:
 doh(float)
 doh(char)
