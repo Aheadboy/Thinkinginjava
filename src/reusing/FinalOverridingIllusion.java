a6577f794df3f065564e0dbf3462e55a691ae6f9
pure <502964379@qq.com>
2019-03-19 14:07:18 +0800

    final method 1、final private方法并没有意义。 2、private方法不会被重写。 3、只有类的接口才会被重写，如public，protected。

--- src/reusing/FinalOverridingIllusion.java
+++ src/reusing/FinalOverridingIllusion.java
@@ -1,11 +1,13 @@
-//: reusing/FinalOverridingIllusion.java
+package reusing;//: reusing/FinalOverridingIllusion.java
 // It only looks like you can override
 // a private or private final method.
 import static net.mindview.util.Print.*;
-
+//只有类的接口才会被重写，private不是类的接口。所以不会被重写。
+//OverridingPrivate2只是定义了新的方法，而不是重写了基类方法。
+//
 class WithFinals {
   // Identical to "private" alone:
-  private final void f() { print("WithFinals.f()"); }
+  private final void f() { print("WithFinals.f()"); }//私有方法final并没有什么实际意义
   // Also automatically "final":
   private void g() { print("WithFinals.g()"); }
 }
