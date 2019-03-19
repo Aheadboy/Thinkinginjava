667e7c58a59fde872f29cf6698329f8b13ad5d96
pure <502964379@qq.com>
2019-03-19 10:56:48 +0800

    final 关键字 1、blank final pdf笔记以及源码注释。 2、定义时不赋值，称为blank final

--- src/reusing/BlankFinal.java
+++ src/reusing/BlankFinal.java
@@ -1,4 +1,4 @@
-//: reusing/BlankFinal.java
+package reusing;//: reusing/BlankFinal.java
 // "Blank" final fields.
 
 class Poppet {
@@ -8,9 +8,9 @@ class Poppet {
 
 public class BlankFinal {
   private final int i = 0; // Initialized final
-  private final int j; // Blank final
+  private final int j; // Blank final、、没有在申明时赋值 的final称为final；即使这里不复制，构造函数那边也必须赋值，初始化。
   private final Poppet p; // Blank final reference
-  // Blank finals MUST be initialized in the constructor:
+  // Blank finals MUST be initialized in the constructor://构造函数这边必须初始化，blank field .
   public BlankFinal() {
     j = 1; // Initialize blank final
     p = new Poppet(1); // Initialize blank final reference
