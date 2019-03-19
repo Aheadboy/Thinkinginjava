6821c66a42c4bc09cbfeccf7db0be9bc8cd9ea11
pure <502964379@qq.com>
2019-03-19 10:31:53 +0800

    final 关键字 1、运行时常量，和非运行时常量 2、命名原则，final以及static+final的命名规则。 3、final也是可以运行时赋值的。 代码注释，以及pdf笔记

--- src/reusing/FinalData.java
+++ src/reusing/FinalData.java
@@ -13,13 +13,18 @@ public class FinalData {
   private String id;
   public FinalData(String id) { this.id = id; }
   // Can be compile-time constants:
-  private final int valueOne = 9;
-  private static final int VALUE_TWO = 99;
+  //编译时常量
+  private final int valueOne = 9;//编译时常量
+  private static final int VALUE_TWO = 99;//编译时常量，static+final命名惯例为大写+下划线。
   // Typical public constant:
-  public static final int VALUE_THREE = 39;
+  public static final int VALUE_THREE = 39;//通常static final用public访问修饰符更为典型。
+
   // Cannot be compile-time constants:
-  private final int i4 = rand.nextInt(20);
-  static final int INT_5 = rand.nextInt(20);
+  //非编译时常量
+  //以下两个差异只有在***运行时***给final赋值时，才会出现。因为编译时常量并不会区别对待final的static和non-static
+  private final int i4 = rand.nextInt(20);//non-static每次实例化新对象都会赋值一次。
+  static final int INT_5 = rand.nextInt(20);//static-类加载时，赋值一次，不会再变。
+
   private Value v1 = new Value(11);
   private final Value v2 = new Value(22);
   private static final Value VAL_3 = new Value(33);
@@ -31,9 +36,9 @@ public class FinalData {
   public static void main(String[] args) {
     FinalData fd1 = new FinalData("fd1");
     //! fd1.valueOne++; // Error: can't change value
-    fd1.v2.i++; // Object isn't constant!
+    fd1.v2.i++; // Object isn't constant!///v2是引用类型，其引用不可修改，但是对象的内容是可以修改的。
     fd1.v1 = new Value(9); // OK -- not final
-    for(int i = 0; i < fd1.a.length; i++)
+    for(int i = 0; i < fd1.a.length; i++)///同上，数组是引用类型，其引用不可修改，但是对象的内容是可以修改的。
       fd1.a[i]++; // Object isn't constant!
     //! fd1.v2 = new Value(0); // Error: Can't
     //! fd1.VAL_3 = new Value(1); // change reference
