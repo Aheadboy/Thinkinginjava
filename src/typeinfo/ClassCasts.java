4decf6a5175a8940e4680d8aca3cda81e053fe7d
pure <502964379@qq.com>
2019-03-20 14:54:18 +0800

    1、类字面常量-ClassName.class获取Class object应用是-懒初始化。

--- src/typeinfo/ClassCasts.java
+++ src/typeinfo/ClassCasts.java
@@ -2,14 +2,17 @@ package typeinfo;
 
 //: typeinfo/ClassCasts.java
 
-class Building {}
-class House extends Building {}
+class Building {
+}
+
+class House extends Building {
+}
 
 public class ClassCasts {
-  public static void main(String[] args) {
-    Building b = new House();
-    Class<House> houseType = House.class;
-    House h = houseType.cast(b);
-    h = (House)b; // ... or just do this.
-  }
+    public static void main(String[] args) {
+        Building b = new House();
+        Class<House> houseType = House.class;
+        House h = houseType.cast(b);//java5在Class object中新增的转型方法。
+        h = (House) b; // ... or just do this.
+    }
 } ///:~
