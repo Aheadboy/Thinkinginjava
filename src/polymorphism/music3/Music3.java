2ca0e47a215c34a4b25d3940fa642560e9d85388
pure <502964379@qq.com>
2019-04-01 21:04:42 +0800

    1、多态、music的例子。面向基类，当其子类泛化类不存在。

--- src/polymorphism/music3/Music3.java
+++ src/polymorphism/music3/Music3.java
@@ -1,65 +1,115 @@
 //: polymorphism/music3/Music3.java
 // An extensible program.
 package polymorphism.music3;
+
 import polymorphism.music.Note;
+
+//多态就是类型间的解耦。
+
 import static net.mindview.util.Print.*;
 
 class Instrument {
-  void play(Note n) { print("Instrument.play() " + n); }
-  String what() { return "Instrument"; }
-  void adjust() { print("Adjusting Instrument"); }
+    void play(Note n) {
+        print("Instrument.play() " + n);
+    }
+
+    String what() {
+        return "Instrument";
+    }
+
+    void adjust() {
+        print("Adjusting Instrument");
+    }
 }
 
 class Wind extends Instrument {
-  void play(Note n) { print("Wind.play() " + n); }
-  String what() { return "Wind"; }
-  void adjust() { print("Adjusting Wind"); }
-}	
+    void play(Note n) {
+        print("Wind.play() " + n);
+    }
+
+    String what() {
+        return "Wind";
+    }
+
+    void adjust() {
+        print("Adjusting Wind");
+    }
+}
 
 class Percussion extends Instrument {
-  void play(Note n) { print("Percussion.play() " + n); }
-  String what() { return "Percussion"; }
-  void adjust() { print("Adjusting Percussion"); }
+    void play(Note n) {
+        print("Percussion.play() " + n);
+    }
+
+    String what() {
+        return "Percussion";
+    }
+
+    void adjust() {
+        print("Adjusting Percussion");
+    }
 }
 
 class Stringed extends Instrument {
-  void play(Note n) { print("Stringed.play() " + n); }
-  String what() { return "Stringed"; }
-  void adjust() { print("Adjusting Stringed"); }
+    void play(Note n) {
+        print("Stringed.play() " + n);
+    }
+
+    String what() {
+        return "Stringed";
+    }
+
+    void adjust() {
+        print("Adjusting Stringed");
+    }
 }
 
 class Brass extends Wind {
-  void play(Note n) { print("Brass.play() " + n); }
-  void adjust() { print("Adjusting Brass"); }
+    void play(Note n) {
+        print("Brass.play() " + n);
+    }
+
+    void adjust() {
+        print("Adjusting Brass");
+    }
 }
 
 class Woodwind extends Wind {
-  void play(Note n) { print("Woodwind.play() " + n); }
-  String what() { return "Woodwind"; }
-}	
+    void play(Note n) {
+        print("Woodwind.play() " + n);
+    }
+
+    String what() {
+        return "Woodwind";
+    }
+}
 
 public class Music3 {
-  // Doesn't care about type, so new types
-  // added to the system still work right:
-  public static void tune(Instrument i) {
-    // ...
-    i.play(Note.MIDDLE_C);
-  }
-  public static void tuneAll(Instrument[] e) {
-    for(Instrument i : e)
-      tune(i);
-  }	
-  public static void main(String[] args) {
-    // Upcasting during addition to the array:
-    Instrument[] orchestra = {
-      new Wind(),
-      new Percussion(),
-      new Stringed(),
-      new Brass(),
-      new Woodwind()
-    };
-    tuneAll(orchestra);
-  }
+    // Doesn't care about type, so new types
+    // added to the system still work right:
+    //不关心具体类型，只面向基类。
+    //因此，新增了基类的新泛化类。也无所谓，程序依然能够正确执行。
+    public static void tune(Instrument i) {
+        // ...
+        i.play(Note.MIDDLE_C);
+    }
+
+    public static void tuneAll(Instrument[] e) {
+        for (Instrument i : e)
+            tune(i);
+    }
+
+    public static void main(String[] args) {
+        // Upcasting during addition to the array:
+        Instrument[] orchestra = {
+                new Wind(),
+                new Percussion(),
+                new Stringed(),
+                new Brass(),
+                new Woodwind()
+        };
+        tuneAll(orchestra);
+    }
 } /* Output:
 Wind.play() MIDDLE_C
 Percussion.play() MIDDLE_C
--- src/reusing/Beetle.java
+++ src/reusing/Beetle.java
@@ -3,6 +3,7 @@ package reusing;//: reusing/Beetle.java
 //加载类首先加载其父类，不管父类有没有被实例化。如果父类还有父类，以此类推
 //加载的时候初始化static代码块，按其在源码中的顺序进行。
 //实例化一个类，先实例化其父类，如果父类还有父类，以此类推。
+
 import static net.mindview.util.Print.*;
 
 class Insect {
@@ -25,6 +26,7 @@ public class Beetle extends Insect {
 
     private static int x2 = printInit("static Beetle.x2 initialized");
     private int k = printInit("Beetle.k initialized");
+
     public Beetle() {
         print("k = " + k);
         print("j = " + j);
